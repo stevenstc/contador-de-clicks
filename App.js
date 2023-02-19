382192c268707f35c6c0185b0710e7f86269bb0d
Steven Cabrera Londoño <32532691+stevenstc@users.noreply.github.com>
2023-02-19 11:26:26 -0500

    ajustes de imagen

--- App.js
+++ App.js
@@ -2,28 +2,29 @@ import React, { useState } from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View,Button } from 'react-native';
 
-
-
-
 export default function App() {
 const [count, setCount] = useState(0);
 
   
   return (
-    <View style={styles.container}>
+    <View onClick={() => setCount(count + 1)} style={styles.container}>
       
-      <Text >Contar Clicks:  {count} </Text> 
+      <Text style={styles.titulo}>Clicks:  {count} </Text> 
+      <br></br>
+      <br></br>
+      <br></br>
+
       <Button
         onPress={() => setCount(count + 1)}
-        title="Tocame"
-        color="#841584"
+        title="Presionar"
+        color="#008000"
         accessibilityLabel="boton para contar"
       />
-
+      <br></br>
       <Button
         onPress={() => setCount(0)}
         title="Resetear"
-        color="#f83"
+        color="#F00"
         accessibilityLabel="boton de reinicio"
       />
       <StatusBar style="auto" />
@@ -34,8 +35,12 @@ const [count, setCount] = useState(0);
 const styles = StyleSheet.create({
   container: {
     flex: 2,
-    backgroundColor: '#0ff',
+    backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  titulo: {
+    fontSize: '30px',
+    fontWeight: '600'
+  }
 });
